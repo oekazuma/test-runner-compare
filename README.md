7ba881d55770a3ecc0724f675a6f775c4407063c
oekazuma <koneko0604@gmail.com>
2021-12-07 03:39:09 +0900

    feat: compare test runners

---
+++ README.md
@@ -0,0 +1,16 @@
+# test-runner-compare
+
+- [Jest](https://github.com/facebook/jest)
+- [AVA](https://github.com/avajs/ava)
+- [Mocha](https://github.com/mochajs/mocha)
+- [tape](https://github.com/substack/tape)
+- [uvu](https://github.com/lukeed/uvu)
+
+## Benchmarks
+```
+~> "jest" 1.02s
+~> "ava" 564ms
+~> "mocha" 194ms
+~> "tape" 118ms
+~> "uvu" 87ms
+```
---
+++ index.js
@@ -0,0 +1,35 @@
+const { promisify } = require('util');
+const { execFile } = require('child_process');
+
+const spawn = promisify(execFile);
+
+const runners = {
+  jest: [require.resolve('./node_modules/jest/bin/jest.js'), './jest', '--env=node'],
+	ava: [require.resolve('./node_modules/ava/cli.js'), './ava/**'],
+	mocha: [require.resolve('./node_modules/mocha/bin/mocha'), './mocha'],
+	tape: [require.resolve('./node_modules/tape/bin/tape'), './tape'],
+	uvu: [require.resolve('./node_modules/uvu/bin.js'), './uvu'],
+};
+
+function format(arr) {
+	let num = Math.round(arr[1] / 1e6);
+	if (arr[0] > 0) return (arr[0] + num / 1e3).toFixed(2) + 's';
+	return `${num}ms`;
+}
+
+async function run(name, args) {
+	let timer = process.hrtime();
+	let pid = await spawn('node', args, { cwd: __dirname });
+	let delta = process.hrtime(timer);
+
+	console.log('~> "%s" %s', name, format(delta));
+}
+
+(async function () {
+	for (let name of Object.keys(runners)) {
+		await run(name, runners[name]);
+	}
+})().catch(err => {
+	console.error('Oops~!', err);
+	process.exit(1);
+});
